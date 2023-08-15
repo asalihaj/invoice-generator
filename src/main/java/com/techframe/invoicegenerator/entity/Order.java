7764e24dd7a84ca329b523e71ac177d35a85698b
Artan Salihaj <artan-s@hotmail.com>
2023-08-15 23:12:53 +0200

    Create constructors, getters and setters for Order class

--- src/main/java/com/techframe/invoicegenerator/entity/Order.java
+++ src/main/java/com/techframe/invoicegenerator/entity/Order.java
@@ -1,8 +1,36 @@
 package com.techframe.invoicegenerator.entity;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
     private String id;
     private List<Invoice> invoices;
+    private BigDecimal subTotal;
+    private BigDecimal vat;
+
+    public Order() {
+        this(new ArrayList<>());
+    }
+
+    public Order(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
 }
